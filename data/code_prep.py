9934d2ecb8e9a88cee67235b13703af89e13c812
tohiddar <tohid.d@gmail.com>
2020-11-19 21:45:07 -0800

    testing gitignore directory

--- data/code_prep.py
+++ data/code_prep.py
@@ -13,3 +13,26 @@ from glob import glob
 from PIL import Image
 import pickle
 
+print('We are here')
+
+# Download caption annotation files
+annotation_folder = '../annotations/'
+if not os.path.exists(os.path.abspath('.') + annotation_folder):
+  annotation_zip = tf.keras.utils.get_file('captions.zip',
+                                          cache_subdir=os.path.abspath('.'),
+                                          origin = 'http://images.cocodataset.org/annotations/annotations_trainval2014.zip',
+                                          extract = True)
+  annotation_file = os.path.dirname(annotation_zip)+'/annotations/captions_train2014.json'
+  os.remove(annotation_zip)
+
+# Download image files
+image_folder = '/train2014/'
+if not os.path.exists(os.path.abspath('.') + image_folder):
+  image_zip = tf.keras.utils.get_file('train2014.zip',
+                                      cache_subdir=os.path.abspath('.'),
+                                      origin = 'http://images.cocodataset.org/zips/train2014.zip',
+                                      extract = True)
+  PATH = os.path.dirname(image_zip) + image_folder
+  os.remove(image_zip)
+else:
+  PATH = os.path.abspath('.') + image_folder
